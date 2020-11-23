f70d3ae912db285adf562af55fe7aa043d142596
maximsmolyakov <sm.maksim@icloud.com>
2020-11-23 22:26:14 +0800

    feat: add ClickableTemplate.kt
    This commit adds an implementation of Template interface for Android applications.

---
+++ androidprinter/src/main/java/com/maximcode/androidprinter/ClickableTemplate.kt
@@ -0,0 +1,20 @@
+package com.maximcode.androidprinter
+
+import android.os.Build
+import com.maximcode.klogger.KLogger
+import com.maximcode.klogger.templates.Template
+
+/**
+ * Implementation of [Template] interface for Android applications.
+ */
+public class ClickableTemplate: Template {
+    override fun apply(vararg args: String): String {
+        val (className, lineNumber, methodName) = args
+        return "${KLogger::class.java.simpleName}: (${shrinkClassName(className)}.kt:$lineNumber).$methodName()"
+    }
+
+    private fun shrinkClassName(className: String) = when {
+        className.length <= 23 || Build.VERSION.SDK_INT >= Build.VERSION_CODES.N -> className
+        else -> className.substring(0, 23)
+    }
+}
